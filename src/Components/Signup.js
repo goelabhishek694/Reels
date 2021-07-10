c8a7f31afea70ff914f0afc20cfcab30055019ac
goelabhishek694 <goelabhishek694@gmail.com>
2021-07-11 03:07:18 +0530

    signup page completed

--- src/Components/Signup.js
+++ src/Components/Signup.js
@@ -1,11 +1,13 @@
 import React,{useState,useEffect,useContext} from 'react'
 import {AuthContext} from "../Context/AuthProvider";
+import{storage,database} from "../firebase";
 function Signup() {
     const[email,setEmail]=useState('');
     const[password,setPassword]=useState('');
     const[name,setName]=useState('');
     const[error,setError]=useState('');
     const[loading,setLoading]=useState(false);
+    const[file,setFile]=useState('');
     const{signup}=useContext(AuthContext);
     console.log(signup);
 
@@ -16,10 +18,64 @@ function Signup() {
         let res= await signup(email,password);
         let uid=res.user.uid;
         console.log(uid);
-        setLoading(false);
+
+        const uploadTaskListener=storage.ref(`users/${uid}/profileImage`).put(file);
+        // Register three observers:
+// 1. 'state_changed' observer, called any time the state changes
+// 2. Error observer, called on failure
+// 3. Completion observer, called on successful completion
+        // fn 1 -> progress tracking
+        // fn2 -> error
+        // fn3 -> success
+        uploadTaskListener.on('state_changed',fn1,fn2,fn3);
+        
+        function fn1(snapshot){
+            var progress = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+            console.log('Upload is ' + progress + '% done');   
         }
-        catch(err){
-            console.log(err);
+
+        function fn2(error){
+            setError(error);
+            setTimeout(()=>{
+                setError('')
+            },2000);
+            setLoading(false);
+        }
+
+        async function fn3(){
+            let downloadUrl=await uploadTaskListener.snapshot.ref.getDownloadURL();
+            console.log(downloadUrl);
+
+            await database.users.doc(uid).set({
+                email:email,
+                userId:uid,
+                username:name,
+                createdAt:database.getCurrentTimeStamp(),
+                profileUrl:downloadUrl,
+                postIds:[]
+            });
+
+            setLoading(false);
+            console.log("User signed in");
+        }
+
+
+        setLoading(false);
+    }
+    catch(error){
+        setError(error);
+        setTimeout(()=>setError(''),2000);
+        setLoading(false);
+    }
+        
+        
+    }
+
+    const handleFileSubmit=(e)=>{
+        let file=e.target.files[0];
+        console.log(file);
+        if(file!=null){
+            setFile(file);
         }
     }
 
@@ -38,6 +94,11 @@ function Signup() {
                     <label htmlFor=''>Password</label>
                     <input type="password" value={password} onChange={(e)=>{setPassword(e.target.value)}}></input>
                 </div>
+                <div>
+                    <label htmlFor='profile'>Profile Photo</label>
+                    <input type="file" accept="image/*" onChange={handleFileSubmit} ></input>
+                </div>
+                <button type='submit'>Login</button>
             </form>
         </div>
     )
