3e7dba01123cdc52c7ef3035fee86a1040f50e32
goelabhishek694 <goelabhishek694@gmail.com>
2021-07-12 03:36:37 +0530

    materialUI poc

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import './App.css';
 import AuthProvider from './Context/AuthProvider';
 import Signup from './Components/Signup';
+import Main from './MaterialUI/Main';
 function App() {
   return (
-   <AuthProvider>
-     <Signup/>
-   </AuthProvider>
+  //  <AuthProvider>
+    
+  //    <Signup/>
+  //  </AuthProvider>
+  <Main/>
   );
 }
 
---
+++ src/MaterialUI/Main.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import Navbar from './Navbar'
+import Buttons from './Buttons'
+function Main() {
+    return (
+        <div>
+            <Navbar/>
+            <Buttons/>
+        </div>
+    )
+}
+
+export default Main
