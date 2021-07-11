2e62eaf7b6cc54fbda410aaef4fc89bb861fa116
goelabhishek694 <goelabhishek694@gmail.com>
2021-07-12 04:00:21 +0530

    materialUI poc

--- src/MaterialUI/Main.js
+++ src/MaterialUI/Main.js
@@ -1,11 +1,13 @@
 import React from 'react'
 import Navbar from './Navbar'
 import Buttons from './Buttons'
+import Cards from './Cards'
 function Main() {
     return (
         <div>
             <Navbar/>
             <Buttons/>
+            <Cards/>
         </div>
     )
 }
